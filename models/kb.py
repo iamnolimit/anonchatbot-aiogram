a0bd41c1ecb5f09909223481d085e223bd4d716d
Keep Calm Stay Relax <101805283+iamnolimit@users.noreply.github.com>
2024-10-31 04:05:11 +0700

    Update kb.py

--- models/kb.py
+++ models/kb.py
@@ -1,22 +1,25 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder,ReplyKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 def gender_select():
     keyboard = InlineKeyboardBuilder()
-    keyboard.button(text="мужской",callback_data="gender_male")
-    keyboard.button(text="женский",callback_data="gender_female")
+    keyboard.button(text="Laki-laki", callback_data="gender_male")
+    keyboard.button(text="Perempuan", callback_data="gender_female")
     return keyboard.as_markup()
 
 def main_menu():
     keyboard = InlineKeyboardBuilder()
-    keyboard.button(text="поиск",callback_data="search_start")
+    keyboard.button(text="Cari Teman Chat", callback_data="search_start")
     return keyboard.as_markup()
+
 def search_menu():
     keyboard = InlineKeyboardBuilder()
-    keyboard.button(text="Остановить поиск", callback_data="search_stop")
+    keyboard.button(text="Berhenti Mencari", callback_data="search_stop")
     keyboard.adjust(1)
     return keyboard.as_markup(resize_keyboard=True)
+
 def chating_menu():
     keyboard = ReplyKeyboardBuilder()
-    keyboard.button(text="Завершить диалог")
-    keyboard.button(text="Поиск новго собеседника")
+    keyboard.button(text="Akhiri Percakapan")
+    keyboard.button(text="Cari Teman Chat Baru")
     keyboard.adjust(1)
+    return keyboard.as_markup(resize_keyboard=True)
